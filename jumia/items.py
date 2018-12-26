421321f4664cf3870bc7d885a40a9c2f97aff901
Sadibou SOW <sadibou.sow@adncorp.com>
2018-12-26 09:55:48 +0000

    first commit

---
+++ jumia/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JumiaItem(scrapy.Item):
+    # define the fields for your item here like:
+    marque = scrapy.Field()
+    modele = scrapy.Field()
+    boite_de_vitesse = scrapy.Field()
+    kilometrage = scrapy.Field()
+    annee = scrapy.Field()
+    type_de_carburant = scrapy.Field() 
+    prix = scrapy.Field()
+    #carosserie = scrapy.Field()
+    #image_url = scrapy.Field()
---
+++ jumia/spiders/jumiaSpider.py
@@ -0,0 +1,74 @@
+import scrapy 
+from jumia.items import JumiaItem
+from scrapy.http import Request
+#from hashlib import sha1
+
+class jumiaSpider(scrapy.Spider):
+    
+    name = 'jumiaSpider'
+    allowed_domains = ['deals.jumia.sn']
+    start_urls = ['https://deals.jumia.sn/voitures']
+
+    
+    
+    def parse_details(self,response):
+        item = JumiaItem()
+        proprietes,cle = response.css('div.new-attr-style>h3>span::text').extract(),response.css('div.new-attr-style>h3::text').extract()
+        cle.pop(0)
+        item['prix'] = response.css('span.price> span::text').extract_first()
+        item['marque'] = response.css('div.new-attr-style>h3>span>a::text').extract_first()
+        if(len(proprietes)==len(cle)==5):
+            item['modele'] = proprietes[0]
+            item['boite_de_vitesse'] = proprietes[1]
+            item['type_de_carburant'] = proprietes[2]
+            item['annee'] = proprietes[3]
+            item['kilometrage'] = proprietes[4]
+        elif(len(proprietes)==len(cle)==4):
+            if(cle[3]=="Année"):
+                item['annee'] = proprietes[3]
+                item['kilometrage'] = "unknown"
+            elif(cle[3]=="Kilométrage") :
+                item['kilometrage'] = proprietes[3]
+                item['annee'] = "unknown"
+            
+        
+        details_vendeur = response.css('div.seller-details')
+        det = details_vendeur.css('dd>span::text').extract()
+        item['date_post'] = details_vendeur.css('dd>time::text').extract_first()
+        # if len(det)==2 :
+        #     item['localisation'] = det[1]
+        #     item['nom_vendeur'] =  det[0]
+        # elif len(det)==1:
+        #         item['localisation'] = det[0]
+        #         item['nom_vendeur'] =response.css('div.user-pic>img::attr(alt)').extract_first()
+        # item['commentaires'] = response.css('div.post-text-content > p::text').extract_first()
+        
+        
+        '''listOfImagesUrl =  response.css('div.slide>img::attr(data-src)').extract()
+        concat_url = ""
+        for imageurl in listOfImagesUrl :
+            result_Of_Hash = sha1(imageurl.encode())
+            concat_url =concat_url+"|"+result_Of_Hash.hexdigest() 
+        
+        item['image_url'] = concat_url'''
+    
+
+        yield item
+
+
+    def parse(self, response):
+        
+
+        base_url = 'https://deals.jumia.sn'
+        cars_urls =  response.css('a.post-link::attr(href)').extract()
+        for car_url in cars_urls : 
+            url = base_url+car_url
+
+            yield Request(url = url,callback=self.parse_details)
+        
+
+        next_page_url = response.css('li.next>a::attr(href)').extract_first()
+
+        if(next_page_url) : 
+        	next_page_url = response.urljoin(next_page_url)
+        	yield scrapy.Request(url = next_page_url,callback = self.parse)
